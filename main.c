e1fc71b6bddadbcc45be6be03f5d10de0f7cdf13
s25y25n <144671761+s25y25n@users.noreply.github.com>
2023-10-05 15:40:02 +0900

    04
    실습4

--- main.c
+++ main.c
@@ -5,10 +5,22 @@ int main(int argc, char *argv[])
 {
     int a ;
     
-    printf("input seconds : ");  
-    scanf("%i", &a);
+    printf("input the year : ");
+    scanf("%d", &a);
     
-    printf("%i seconds - %i:%i \n", a, a/60, a%60);
+    //1. 4로 나눴을때 나머지 0
+    //2. 100으로 나눴을때 나머지가 0이 아님
+    //3. 400으로 나넜을때 나머지 0 
+    
+    if( (a % 4 == 0 && a %100 != 0) || a % 400 == 0 ) 
+    {
+         printf("is the year %d the leap year? : Yes\n", a);
+    }
+    else
+    {
+           printf("is the year %d the leap year? : No\n", a);
+    }
+         
       
   system("PAUSE");	
   return 0;
